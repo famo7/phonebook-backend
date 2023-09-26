9aaceed19e49f91527ab3e5c227238a7361aaaad
famo1901 <famo1901@gmail.com>
2023-09-26 16:49:41 +0200

    uibuild

--- index.js
+++ index.js
@@ -17,59 +17,102 @@ app.get('/api/persons', (req, res) => {
     res.json(people);
   });
 });
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  let person = people.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  people = people.filter((person) => person.id !== id);
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
 
-  response.status(204).end();
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  const maxId = people.length > 0 ? Math.max(...people.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'content missing',
     });
   }
-  const personFound = people.find((p) => p.name == body.name);
+  let personExist = false;
+  Person.find({ name: body.name }).then((found) => {
+    personExist = true;
+  });
 
-  if (personFound) {
+  if (personExist) {
     return response.status(400).json({
       error: 'name must be unique',
     });
   }
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  people = people.concat(person);
-
-  response.json(person);
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.get('/info', (req, res) => {
-  const count = people.length;
-  const date = new Date().toString();
-  res.send(`<p>phonebook has info for ${count} people</p><p>${date}</p>`);
+  Person.count({}).then((c) => {
+    const date = new Date().toString();
+    res.send(`<p>phonebook has info for ${c} people</p><p>${date}</p>`);
+  });
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
--- models/person.js
+++ models/person.js
@@ -17,8 +17,22 @@ mongoose
   });
 
 const phonebookSchema = new mongoose.Schema({
-  name: String,
-  phone: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^(?:\d{2,3}-\d+){1,}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 phonebookSchema.set('toJSON', {
   transform: (document, returnedObject) => {
